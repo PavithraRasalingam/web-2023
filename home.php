169152ae6809992e70228c7de1cb232a0afa24bc
PavithraRasalingam <149559707+PavithraRasalingam@users.noreply.github.com>
2024-01-09 00:28:03 +0530

    Home_page

---
+++ home.php
@@ -0,0 +1,73 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width,initial-scale=1">
+	
+	<title>Home </title>
+	<link rel="stylesheet" type="text/css" href="Home/css/homepage.css">
+    <link rel="stylesheet" type="text/css" href="Home/css/bootstrap.min.css">
+	
+</head>
+<body>
+	
+	<div class="header">
+	<img src="Home/images.png" content="width=device-width,initial-scale=1">
+    </div>
+
+    <nav class="navbar navbar-dark bg-dark fixed-center">
+  <div class="container-fluid">
+    <button class="navbar-toggler" type="button" data-bs-toggle="offcanvas" data-bs-target="#offcanvasDarkNavbar" aria-controls="offcanvasDarkNavbar" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="offcanvas offcanvas-start text-bg-dark" tabindex="-1" id="offcanvasDarkNavbar" aria-labelledby="offcanvasDarkNavbarLabel">
+      <div class="offcanvas-header">
+                <button type="button" class="btn-close btn-close-white" data-bs-dismiss="offcanvas" aria-label="Close"></button>
+      </div>
+      <div class="offcanvas-body">
+        <ul class="navbar-nav justify-content-center flex-grow-1 pe-3">
+          <li class="nav-item">
+            <a class="nav-link active" aria-current="page" href="">Admin</a>
+          </li>
+           <li class="nav-item">
+            <a class="nav-link active" aria-current="page" href="">Student</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link active" aria-current="page" href="">View Result</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link active" aria-current="page" href="">About </a>
+          </li>
+        </ul>
+      </div>
+    </div>
+  </div>
+</nav>
+    <div class="body">
+    	<div class="letter">
+    		<br>
+    		<br>
+    		<p>&nbsp</p>
+    		<p>&nbsp</p>
+    		
+    		<p>
+    			 <b style="color:gold; font-size:50px ;;">WELCOME TO APTITUDE TEST <br>
+           REGISTATION PORTAL UNIVERSITY OF 
+           <br>VAVUNIYA</b>
+    			<br>
+
+    			<br><b style=" font-size: 30px;"><u>
+    			Calling for Aptitude(2023/2024)<br>
+    	A/L 2022 batch <br> 
+    		</u></b>
+    	</div>
+    	
+    </div>
+
+   
+
+    <script type="text/javascript" src="bootstrap/js/bootstrap.bundle.min.js"></script>
+</body>
+</html>
+
+
